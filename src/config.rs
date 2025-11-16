8b1593535f928fe6093612a3bb56627691923f43
xiehan12 <97071572+xiehan12@users.noreply.github.com>
2025-11-17 05:37:01 +0800

    chore: update rendezvous server to custom server rust.lxnt.cc

--- src/config.rs
+++ src/config.rs
@@ -109,8 +109,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.lxnt.cc"];
+pub const RS_PUB_KEY: &str = "pkoe1dH11copECrf8K6k7Dq+mYLw4lL6DO2yNmgrpwo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
